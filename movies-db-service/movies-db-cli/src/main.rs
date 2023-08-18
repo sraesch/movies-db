302313d589da33b7d6429d351acc112d47adb984
sascha-raesch <sascha.raesch@threedy.io>
2023-08-18 07:01:16 +0200

    added: basic logging and program options

--- movies-db-service/movies-db-cli/src/main.rs
+++ movies-db-service/movies-db-cli/src/main.rs
@@ -1,3 +1,43 @@
+mod options;
+
+use anyhow::Result;
+use log::{error, info};
+use options::Options;
+
+use clap::{Parser};
+
+use log::{LevelFilter};
+
+/// Parses the program arguments and returns None, if no arguments were provided and Some otherwise.
+fn parse_args() -> Result<Options> {
+    let options = Options::parse();
+    Ok(options)
+}
+
+/// Initializes the program logging
+fn initialize_logging(filter: LevelFilter) {
+    simple_logging::log_to(std::io::stdout(), filter);
+}
+
+
+/// Runs the program.
+fn run_program() -> Result<()> {
+    let options = parse_args()?;
+    initialize_logging(LevelFilter::from(options.log_level));
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    match run_program() {
+        Ok(()) => {
+            info!("SUCCESS");
+        }
+        Err(err) => {
+            error!("Error: {}", err);
+            error!("FAILED");
+
+            std::process::exit(-1);
+        }
+    }
 }
---
+++ movies-db-service/movies-db-cli/src/options.rs
@@ -0,0 +1,39 @@
+use std::path::PathBuf;
+
+use clap::{Parser, ValueEnum};
+use log::LevelFilter;
+
+#[derive(ValueEnum, Clone, Copy, Debug)]
+pub enum LogLevel {
+    Trace,
+    Debug,
+    Info,
+    Warn,
+    Error,
+}
+
+impl From<LogLevel> for LevelFilter {
+    fn from(value: LogLevel) -> Self {
+        match value {
+            LogLevel::Trace => LevelFilter::Trace,
+            LogLevel::Debug => LevelFilter::Debug,
+            LogLevel::Info => LevelFilter::Info,
+            LogLevel::Warn => LevelFilter::Warn,
+            LogLevel::Error => LevelFilter::Error,
+        }
+    }
+}
+
+
+/// CLI interface to test different occlusion culler algorithms.
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+pub struct Options {
+    /// The log level
+    #[arg(short, value_enum, long, default_value_t = LogLevel::Info)]
+    pub log_level: LogLevel,
+
+    /// The path to the root directory
+    #[arg(short, long)]
+    pub root_dir: PathBuf,
+}
