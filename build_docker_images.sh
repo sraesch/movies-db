1660116828e7c22b656070933ea822613863c5ce
sascha-raesch <sascha.raesch@threedy.io>
2023-08-23 12:21:27 +0200

    added: build rule for gateway docker image

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 docker build -t movies-db-service -f docker/Dockerfile.movies-db-service .
 docker build -t movies-db-ui -f docker/Dockerfile.movies-db-ui .
+docker build -t movies-db-gateway -f docker/Dockerfile.movies-db-gateway .
